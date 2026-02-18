11a452bbb6c73f412574dea10e2e1c9d212989a3
Jay Barrios <reyezsj@gmail.com>
2026-02-18 11:05:52 -0800

    commit 2

---
+++ src/pages/cats.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Cats() {
+     return (
+          <div className="cats-page">
+               <h1>Cats</h1>
+               <p>Welcome to the cats page!</p>
+          </div>
+     );
+}
+
