d83870427602e4902fe04b97e98bf0f0365161f6
Jay Barrios <reyezsj@gmail.com>
2026-02-18 11:11:08 -0800

    added link to cats

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import cats from './pages/cats'
+import Cats from './pages/cats'
+import { Link } from 'react-router-dom';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,7 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <cats />
+      <Link to="/cats">Go to Cats Page</Link>
     </>
 
     
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Cats from './pages/cats'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/cats" element={<Cats />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
